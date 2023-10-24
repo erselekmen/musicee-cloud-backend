2957fb35ac9437c853ea4f23181e7118e3f487e8
Ersel Ekmen <info@ratic.io>
2023-10-24 21:05:24 +0300

    get and post items endpoints added

--- app/app.py
+++ app/app.py
@@ -1,16 +1,27 @@
+from http.client import HTTPException
 from fastapi import FastAPI
 from app.db import get_database, close_database
+from pydantic import BaseModel
+from app import Item
 
 app = FastAPI()
-
-@app.on_event("startup")
-async def startup_event():
-    await get_database()
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    await close_database()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item):
+    existing_item = await app.mongodb.items.find_one({"name": item.name})
+    if existing_item:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    await app.mongodb.items.insert_one(item.dict())
+    return item
+
+@app.get("/items/{item_name}", response_model=Item)
+async def read_item(item_name: str):
+    item = await app.mongodb.items.find_one({"name": item_name})
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
--- app/db.py
+++ app/db.py
@@ -1,7 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
 
 DATABASE_URL = "mongodb+srv://erselekmen:VV4J5jV9&q*3XrY@cluster0.eh8uyv7.mongodb.net/?retryWrites=true&w=majority"  # Using the Docker service name `mongo`
 database = None
+
+class Item(BaseModel):
+    name: str
+    description: str
 
 async def get_database():
     global database
