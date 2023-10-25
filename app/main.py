c13a0259dd2fa96cac730592dd94e1bfba942edf
Ersel Ekmen <info@ratic.io>
2023-10-25 18:53:39 +0300

    BaseModel inheritance added to Item class

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 DATABASE_URL = "mongodb+srv://erselekmen:VV4J5jV9&q*3XrY@cluster0.eh8uyv7.mongodb.net/?retryWrites=true&w=majority"  # Using the Docker service song `mongo`
 database = None
 
-class Item:
+class Item(BaseModel):
     song: str
     artist: str
 
