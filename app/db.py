f2dab688f0537b648506f34a2b97b6ee71bb2b34
Ersel Ekmen <info@ratic.io>
2023-10-25 18:20:53 +0300

    .dockerignore and db.py added

---
+++ app/db.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, status, Depends, HTTPException
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from main import Item
+
+DATABASE_URL = "mongodb+srv://erselekmen:VV4J5jV9&q*3XrY@cluster0.eh8uyv7.mongodb.net/?retryWrites=true&w=majority"  # Using the Docker service song `mongo`
+database = None
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(DATABASE_URL)
+    app.mongodb = app.mongodb_client.ItemDB
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
--- app/main.py
+++ app/main.py
@@ -1,26 +1,13 @@
-from http.client import HTTPException
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Depends, HTTPException
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseModel
-
-DATABASE_URL = "mongodb+srv://erselekmen:VV4J5jV9&q*3XrY@cluster0.eh8uyv7.mongodb.net/?retryWrites=true&w=majority"  # Using the Docker service song `mongo`
-database = None
-
 
 app = FastAPI()
+
 class Item(BaseModel):
     song: str
     artist: str
 
-@app.on_event("startup")
-async def startup_db_client():
-    app.mongodb_client = AsyncIOMotorClient(DATABASE_URL)
-    app.mongodb = app.mongodb_client.ItemDB
-
-@app.on_event("shutdown")
-async def shutdown_db_client():
-    app.mongodb_client.close()
 
 @app.post("/add_song/", response_model=Item)
 async def create_item(item: Item):
