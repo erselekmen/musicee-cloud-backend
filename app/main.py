ccfdf7dcb7caaa56fdde0aebf9b2a5606d20338a
Ersel Ekmen <info@ratic.io>
2023-10-26 20:19:12 +0300

    database configs moved from main to db module

--- app/main.py
+++ app/main.py
@@ -1,24 +1,16 @@
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from fastapi import FastAPI, status, Depends, HTTPException
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
+from app.db import *
 
-DATABASE_URL = "mongodb+srv://erselekmen:VV4J5jV9&q*3XrY@cluster0.eh8uyv7.mongodb.net/?retryWrites=true&w=majority"  # Using the Docker service song `mongo`
-database = None
 
 class Item(BaseModel):
     song: str
     artist: str
-
-app = FastAPI()
-
-@app.on_event("startup")
-async def startup_db_client():
-    app.mongodb_client = AsyncIOMotorClient(DATABASE_URL)
-    app.mongodb = app.mongodb_client.ItemDB
-
-@app.on_event("shutdown")
-async def shutdown_db_client():
-    app.mongodb_client.close()
 
 @app.post("/add_song/", response_model=Item)
 async def create_item(item: Item):
