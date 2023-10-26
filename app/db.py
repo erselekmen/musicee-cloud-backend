4230d97d604ad926c3a3969e18435ac1fa4628cb
Ersel Ekmen <info@ratic.io>
2023-10-26 22:11:48 +0300

    comment removed

--- app/db.py
+++ app/db.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI, status, Depends, HTTPException
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
 
-DATABASE_URL = "mongodb+srv://erselekmen:VV4J5jV9&q*3XrY@cluster0.eh8uyv7.mongodb.net/?retryWrites=true&w=majority"  # Using the Docker service song `mongo`
+DATABASE_URL = "mongodb+srv://erselekmen:VV4J5jV9&q*3XrY@cluster0.eh8uyv7.mongodb.net/?retryWrites=true&w=majority"
 database = None
 
 app = FastAPI()
-
 
 @app.on_event("startup")
 async def startup_db_client():
