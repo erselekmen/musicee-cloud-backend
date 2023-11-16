dfaa502857aee8a552073bba0b765aa4dcfc1ed2
erselekmen <erselekmen27@gmail.com>
2023-11-16 15:44:14 +0300

    get_all_tracks & add_track endpoints added

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from app.utils import (
 )
 
 
-@app.post('/signup', summary="Create new user")
+@app.post('/user/signup', summary="Create new user")
 async def create_user(data: User):
     # querying database to check if user already exist
     existing_user = await app.mongodb.users.find_one({"email": data.email})
@@ -31,7 +31,7 @@ async def create_user(data: User):
     }
 
 
-@app.post('/login', summary="Create access and refresh tokens for user")
+@app.post('/user/login', summary="Create access and refresh tokens for user")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = await app.mongodb.users.find_one({"email": form_data.username})
     if user is None:
@@ -51,3 +51,45 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
         "access_token": create_access_token(user['email']),
         "refresh_token": create_refresh_token(user['email']),
     }
+
+
+@app.get('/tracks/get_tracks', summary="List all tracks")
+async def get_tracks():
+
+    cursor = app.mongodb.tracks.find({})
+
+    if cursor is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="No track was found!"
+        )
+
+    tracks = []
+    async for document in cursor:
+
+        del document['_id']
+        tracks.append(document)
+
+    return tracks
+
+@app.post("/tracks/add_track", summary="Add a track")
+async def add_track(data: Track):
+
+    if data is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Please check track information!"
+        )
+
+    track = {
+        "track_name": data.track_name,
+        "track_artist": data.track_artist,
+        "track_album": data.track_album,
+        "track_rating": data.track_rating,
+        "track_id": data.track_id,
+        "track_release_year": data.track_release_year
+    }
+
+    await app.mongodb.tracks.insert_one(track)
+
+    return {"message": "Track added successfully."}
