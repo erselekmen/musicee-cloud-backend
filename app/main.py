7317797891aa481dfeecf4d9dd90fd97587d2590
erselekmen <erselekmen27@gmail.com>
2023-11-18 02:36:34 +0300

    update_track endpoint added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi.exception_handlers import HTTPException
 from fastapi import Depends, status
-from fastapi.security import OAuth2PasswordRequestForm
+from pymongo import ReturnDocument
 from app.db import *
 from app.schema import *
 from app.utils import (
@@ -18,12 +18,12 @@ async def create_user(data: User):
 
     if existing_user:
         raise HTTPException(status_code=400, detail="Item already exists")
-    
+
     user = {
         "email": data.email,
-        "password": get_hashed_password(data.password) 
+        "password": get_hashed_password(data.password)
     }
-    
+
     await app.mongodb.users.insert_one(user)
     return {
         "email": data.email,
@@ -46,7 +46,7 @@ async def login(data: User):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect email or password"
         )
-    
+
     return {
         "access_token": create_access_token(user['email']),
         "refresh_token": create_refresh_token(user['email']),
@@ -55,7 +55,6 @@ async def login(data: User):
 
 @app.get('/tracks/get_tracks', summary="List all tracks")
 async def get_tracks():
-
     cursor = app.mongodb.tracks.find({})
 
     if cursor is None:
@@ -66,15 +65,14 @@ async def get_tracks():
 
     tracks = []
     async for document in cursor:
-
         del document['_id']
         tracks.append(document)
 
     return tracks
 
+
 @app.post("/tracks/add_track", summary="Add a track")
 async def add_track(data: Track):
-
     if data is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -93,3 +91,23 @@ async def add_track(data: Track):
     await app.mongodb.tracks.insert_one(track)
 
     return {"message": "Track added successfully."}
+
+
+@app.put("/tracks/update_track/{track_id}", summary="Update track")
+async def update_track(data: Track, track_id: int):
+
+    await app.mongodb.tracks.find_one_and_update(
+        {"track_id": track_id},
+        {"$set":
+            {
+                "track_name": data.track_name,
+                "track_artist": data.track_artist,
+                "track_album": data.track_album,
+                "track_rating": data.track_rating,
+                "track_release_year": data.track_release_year
+            }
+        },
+        return_document=ReturnDocument.AFTER
+    )
+
+    return {"message": f"Track with ID {track_id} has been updated."}
