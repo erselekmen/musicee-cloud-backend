e6ddf03a09794b7deac3b1d23837fb8902ff15c3
erselekmen <erselekmen27@gmail.com>
2024-04-22 13:39:43 +0300

    next line added

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ API_URL = "http://musicee.us-west-2.elasticbeanstalk.com"
 
 @app.get("/api/health")
 def root():
-    return {"message": "Welcome to Musicee API"}
+    return {"message": "Welcome to Musicee API\n"}
 
 
 @app.post('/user/signup', summary="Create new user")
